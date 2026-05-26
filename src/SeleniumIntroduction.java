7c650b220c340c3e21735ab32f825193aafa4e3d
aditi verma <aditiv271@gmail.com>
2026-05-26 23:00:27 +0530

    Learning Selenium

---
+++ src/SeleniumIntroduction.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntroduction {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.com.driver","C:\\Users\\aditi verma\\Documents\\chromedriver-win64.exe");
+		// invoke chrome browser
+		WebDriver driver=new ChromeDriver();
+		
+
+	}
+
+}
